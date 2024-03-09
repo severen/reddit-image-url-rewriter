d4ae82bc889111b1305f9620e6ba95c40867be14
Severen Redwood <sev@severen.dev>
2024-03-09 23:29:20 +1300

    Redirect Imgur URLs too

--- src/background.js
+++ src/background.js
@@ -1,4 +1,4 @@
-function redirect(requestDetails) {
+function redirectReddit(requestDetails) {
   const paramPos = requestDetails.url.indexOf("?");
   const url =
     paramPos == -1 ? requestDetails.url : requestDetails.url.substring(0, paramPos);
@@ -11,12 +11,25 @@ function redirect(requestDetails) {
 }
 
 chrome.webRequest.onBeforeRequest.addListener(
-  redirect,
+  redirectReddit,
   // This should ensure that the redirect function only runs when a request to
   // Reddit's preview domain is sent for a main frame (i.e. as a top-level
   // document loaded into a tab). This way we avoid interfering with the
   // functionality of Reddit by rewriting its requests.
   { urls: ["*://preview.redd.it/*"], types: ["main_frame"] },
+  // Make the listener synchronous and thereby allow requests to be modified.
+  ["blocking"],
+);
+
+function redirectImgur(requestDetails) {
+  return {
+    redirectUrl: requestDetails.url.replace(/\.gifv/, ".mp4"),
+  };
+}
+
+chrome.webRequest.onBeforeRequest.addListener(
+  redirectImgur,
+  { urls: ["*://i.imgur.com/*"], types: ["main_frame"] },
   // Make the listener synchronous and thereby allow requests to be modified.
   ["blocking"],
 );
